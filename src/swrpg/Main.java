c986c41885b84975bccca53dc8835c2baeb56be1
rustadmd <markrustad05@gmail.com>
2014-10-31 17:39:44 -0500

    initial commit

---
+++ src/swrpg/Main.java
@@ -0,0 +1,28 @@
+/**
+ * @author Mark Rustad
+ * @version .01
+ * @date Oct 27, 2014
+ */
+package swrpg;
+
+import swrpg.Settings;
+
+/**
+ * @author Mark
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	
+	@SuppressWarnings("unused")
+	private static Settings s;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		s = new Settings();
+	}
+
+}
---
+++ src/swrpg/Settings.java
@@ -0,0 +1,102 @@
+/**
+ * 
+ */
+package swrpg;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.sql.Driver;
+import java.sql.DriverManager;
+
+import swrpg.Database.DriverShim;
+
+/**
+ * @author Mark Rustad
+ * @version .01
+ * @date May 19, 2014
+ *
+ */
+public class Settings {
+	
+	private static String m_programPath;
+	private static String m_databasePath;
+	private static String m_imagePath;
+	//public static FullCardSet fcs; //there is only one master list of cards
+	
+	/**
+	 * Creates initial settings, sets up file path and SQLlite connector
+	 */
+	public Settings()
+	{
+		m_programPath = setProgramPath();
+		setupSqlLiteConnector();
+	}
+	
+	/**
+	 * Gets the current program file location
+	 * @return
+	 */
+	public static String getProgramPath()
+	{
+		return m_programPath;
+	}
+	
+	public static String getImagePath()
+	{
+		return m_imagePath;
+	}
+	
+	/**
+	 * Sets the program file location
+	 * @return
+	 */
+	private String setProgramPath()
+	{
+		//Find the current class path
+		URL location = getClass().getProtectionDomain().getCodeSource().getLocation();
+		//System.out.println(location);
+		String programPathWithFile = location.toString();
+		String programPath = programPathWithFile.substring(5);//delete "file:"
+		//System.out.println(programPath);//debugging
+		
+		//set resource locations
+		String resourceLocation = "resources/Database/";
+		m_databasePath = programPath + resourceLocation;
+		m_imagePath = programPath + "resources/Images";
+		return programPath;
+		
+	}
+	
+	public static String getDatabasePath()
+	{
+		return m_databasePath;
+	}
+	
+	/**
+	 * Sets up the Sqllite db connector
+	 */
+	private void setupSqlLiteConnector()
+	{
+		
+		//Load the JDBC Connector
+		String jarPrefix = "jar:file:";
+		String connectorName = "sqlite-jdbc-3.7.2.jar";
+		String driverName = "org.sqlite.JDBC";
+		
+		URL u = null;
+		
+		try {
+			u = new URL(jarPrefix + m_databasePath + connectorName + "!/");
+			URLClassLoader ucl = new URLClassLoader(new URL[] { u });
+			Driver d = (Driver)Class.forName(driverName, true, ucl).newInstance();
+			DriverManager.registerDriver(new DriverShim(d));
+			//System.out.println(u.toString());//debugging
+		} catch (Exception e) {
+			System.out.println("Database connector could not be established: " + u);
+			e.printStackTrace();
+		} 
+		
+	}
+	
+
+}
