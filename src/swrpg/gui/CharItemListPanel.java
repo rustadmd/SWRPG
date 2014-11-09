9240762f1410ccab9392a7d3c1ede8e1583d14ce
rustadmd <markrustad05@gmail.com>
2014-11-08 18:30:42 -0600

    Item list display issue corrected.

---
+++ src/swrpg/gui/CharItemListPanel.java
@@ -0,0 +1,81 @@
+/**
+ * @author Mark Rustad
+ * @version .01
+ * @date Nov 8, 2014
+ */
+package swrpg.gui;
+
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
+
+import swrpg.model.CharItem;
+
+/**
+ * @author Mark
+ *
+ */
+public class CharItemListPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2752539691315991690L;
+	
+	public CharItemListPanel(ArrayList<CharItem> list)
+	{
+		DefaultTableModel model = new DefaultTableModel();
+		JTable table = new JTable(model);
+		
+		//set up columns
+		//model.setColumnIdentifiers(columnNames);
+		model.addColumn("Item");
+		model.addColumn("Count");
+		TableColumnModel columns = table.getColumnModel();
+		TableColumn nameColumn = columns.getColumn(0);
+		//nameColumn.setHeaderValue("Item");
+		nameColumn.setMinWidth(150);
+		
+		TableColumn countColumn = columns.getColumn(1);
+		//countColumn.setHeaderValue("Amount");
+		countColumn.setPreferredWidth(20);
+		
+	
+		//fill the table
+		Iterator<CharItem> i = list.iterator();
+		while (i.hasNext())
+		{
+			CharItem ci = i.next();
+			Object[] row = {ci.getItem().getName(), ci.getCount()};
+			model.addRow(row);
+		}
+		JScrollPane scrollpane = new JScrollPane(table, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		add(scrollpane);
+		
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		
+		//Size issues
+		Dimension d = new Dimension();
+		d.height = 200;
+		d.width = 300;
+		table.setPreferredScrollableViewportSize(d);
+
+		//System.out.println(table.getPreferredScrollableViewportSize().getHeight());
+		//System.out.println(table.getPreferredScrollableViewportSize().getWidth());
+		
+		//JScrollBar bar = new JScrollBar();
+		//bar.setVisible(true);
+		//scrollpane.setVerticalScrollBar(bar);
+	}
+
+}
--- src/swrpg/gui/CharacterPanel.java
+++ src/swrpg/gui/CharacterPanel.java
@@ -5,6 +5,7 @@
  */
 package swrpg.gui;
 
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
@@ -68,7 +69,7 @@ public class CharacterPanel extends JPanel {
 		add(tabs, tabConstraints);
 		
 		addSkillsTab(tabs);
-		
+		addItemsTab(tabs);
 		}
 	
 	private void addSkillsTab(JTabbedPane p)
@@ -100,4 +101,25 @@ public class CharacterPanel extends JPanel {
 		p.addTab("Skills", skillsPanel);
 	}
 
+	private void addItemsTab(JTabbedPane p)
+	{
+		JPanel itemsPanel = new JPanel();
+		itemsPanel.setLayout(new GridBagLayout());
+		
+		int credits = c.getCredits();
+		FieldDisplay creditDisplay = new FieldDisplay("Credits", Integer.toString(credits));
+		GridBagConstraints creditConstraints = new GridBagConstraints();
+		creditConstraints.gridx = 0;
+		creditConstraints.gridy = 0;
+		itemsPanel.add(creditDisplay, creditConstraints);
+		
+		//add the item list
+		CharItemListPanel itemList = new CharItemListPanel(c.getItemList());
+		GridBagConstraints itemListConstraints = new GridBagConstraints();
+		itemListConstraints.fill = GridBagConstraints.BOTH;
+		itemListConstraints.gridx = 0;
+		itemListConstraints.gridy = 1;
+		itemsPanel.add(itemList, itemListConstraints);
+		p.addTab("Items", itemsPanel);
+	}
 }
